36ff405e50488168d5053c9c0867f3691e4b51b4
Martin Stamm <martin.stamm@camunda.com>
2024-03-18 14:51:25 +0100

    chore: migrate to latest ContextPad version

--- src/features/CustomContextPadProvider.js
+++ src/features/CustomContextPadProvider.js
@@ -3,14 +3,14 @@ import { query as domQuery } from 'min-dom';
 
 export default class CustomContextPadProvider {
   constructor(contextPad, refactorings, suggestionsOverlays, popupMenu, refactoringActionsEntryProvider, eventBus) {
-    contextPad.registerProvider(100, this);
+    contextPad.registerProvider(10, this);
 
     this._refactorings = refactorings;
     this._suggestionOverlays = suggestionsOverlays;
     this._popupMenu = popupMenu;
     this._refactoringActionsEntryProvider = refactoringActionsEntryProvider;
 
-    eventBus.on('contextPad.open', this._renderEntry.bind(this));
+    eventBus.on('contextPad.open', 100, this._renderEntry.bind(this));
   }
 
   _getPopupMenuPosition(target) {
@@ -42,7 +42,7 @@ export default class CustomContextPadProvider {
     this._popupMenu.open(element, 'refactoring-actions',
       this._getPopupMenuPosition(pad),
       {
-        title: 'Apply Refactorings',
+        title: 'Apply refactorings',
         width: 350
       }
     );
@@ -52,11 +52,12 @@ export default class CustomContextPadProvider {
   // In the future, we want the context pad to support Preact components natively
   _renderEntry({ current }) {
     const placeHolder = domQuery('#RefactoringActionPlaceholder', current.pad.html);
+    const group = domQuery('.group[data-group="suggest-refactoring"]', current.pad.html);
 
     if (placeHolder) {
       render(html`<${RefactoringActionItem}
         element=${current.target}
-        setClass=${(name) => {placeHolder.parentElement.classList.toggle(name);}}
+        setClass=${(name) => { group.classList.toggle(name);}}
         refactoringActionsEntryProvider=${this._refactoringActionsEntryProvider}
         openPopupMenu=${() => this._openPopupMenu(current.pad.html, current.target)}
       />`, placeHolder
@@ -88,11 +89,14 @@ function RefactoringActionItem(props) {
     const load = async () => {
       const suggestedRefactoring = await refactoringActionsEntryProvider.fetchRefactoringActions(element);
 
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
       if (suggestedRefactoring) {
         setLoadingState('LOADED');
         setClass('active');
       } else {
         setLoadingState('EMPTY');
+        setClass('empty');
       }
     };
 
@@ -101,27 +105,31 @@ function RefactoringActionItem(props) {
 
   return html`
     ${loadingState === 'LOADING' ? html`<${LoadingIcon} />` : null}
-    ${loadingState === 'EMPTY' ? html`<${EmptyIcon}/>` : null}
+    ${loadingState === 'EMPTY' ? html`<${RefactoringIcon} domclass="no-refactorings-available" title="No suggestions available"/>` : null}
     ${loadingState === 'LOADED' ? html`<div onClick=${openPopupMenu} role="button">
-        <${RefactoringIcon} />
+        <${RefactoringIcon} title="Suggest refactoring"/>
       </div>` : null}
   `;
 }
 
 function RefactoringIcon(props) {
-  return html`<div class="entry" title="Suggest refactoring" >
-                <svg id="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                  <defs>
-                    <style>.cls-1{fill:none;}</style>
-                  </defs>
-                  <title>Suggest refactoring</title>
-                  <path fill="white" d="M29.4141,24,12,6.5859a2.0476,2.0476,0,0,0-2.8281,0l-2.586,2.586a2.0021,2.0021,0,0,0,0,2.8281L23.999,29.4141a2.0024,2.0024,0,0,0,2.8281,0l2.587-2.5865a1.9993,1.9993,0,0,0,0-2.8281ZM8,10.5859,10.5859,8l5,5-2.5866,2.5869-5-5ZM25.4131,28l-11-10.999L17,14.4141l11,11Z"/>
-                  <rect fill="white" x="2.5858" y="14.5858" width="2.8284" height="2.8284" transform="translate(-10.1421 7.5147) rotate(-45)"/>
-                  <rect fill="white" x="14.5858" y="2.5858" width="2.8284" height="2.8284" transform="translate(1.8579 12.4853) rotate(-45)"/>
-                  <rect fill="white" x="2.5858" y="2.5858" width="2.8284" height="2.8284" transform="translate(-1.6569 4) rotate(-45)"/>
-                  <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/>
-                </svg>
-              </div>`;
+  const {
+    title,
+    domclass = ''
+  } = props;
+
+  return html`<div class="entry ${domclass}" title="${title}" >
+  <svg id="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+  <defs>
+    <style>.cls-1{fill:none;}</style>
+  </defs>
+  <title>${title}</title>
+  <path fill="currentcolor" d="M29.4141,24,12,6.5859a2.0476,2.0476,0,0,0-2.8281,0l-2.586,2.586a2.0021,2.0021,0,0,0,0,2.8281L23.999,29.4141a2.0024,2.0024,0,0,0,2.8281,0l2.587-2.5865a1.9993,1.9993,0,0,0,0-2.8281ZM8,10.5859,10.5859,8l5,5-2.5866,2.5869-5-5ZM25.4131,28l-11-10.999L17,14.4141l11,11Z"/>
+  <rect fill="currentcolor" x="2.5858" y="14.5858" width="2.8284" height="2.8284" transform="translate(-10.1421 7.5147) rotate(-45)"/>
+  <rect fill="currentcolor" x="14.5858" y="2.5858" width="2.8284" height="2.8284" transform="translate(1.8579 12.4853) rotate(-45)"/>
+  <rect fill="currentcolor" x="2.5858" y="2.5858" width="2.8284" height="2.8284" transform="translate(-1.6569 4) rotate(-45)"/>
+  <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/>
+</svg></div>`;
 }
 
 function LoadingIcon(props) {
@@ -129,9 +137,9 @@ function LoadingIcon(props) {
   <div class="cds--layout">
 <div class="cds--loading cds--loading--small">
   <svg viewBox="0 0 100 100" class="cds--loading__svg">
-    <title>Loading</title> 
-    <circle cx="50%" cy="50%" class="cds--loading__stroke" r="44"></circle>
-  </svg>
+  <title>Loading</title> 
+  <circle cx="50%" cy="50%" class="cds--loading__stroke" r="40"></circle>
+</svg>
 </div>
 </div>`;
 }
