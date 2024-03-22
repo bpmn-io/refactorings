b5fcfe30488bb55d33586c2a4a330b5f4b877f84
Martin Stamm <martin.stamm@camunda.com>
2024-03-22 12:05:31 +0100

    feat: move loading to popup menu

--- test/spec/popup-menu/RefactoringsPopupMenuProvider.spec.js
+++ test/spec/popup-menu/RefactoringsPopupMenuProvider.spec.js
@@ -4,6 +4,7 @@ import {
 } from 'test/TestHelper';
 
 import {
+  query as domQuery,
   queryAll as domQueryAll,
 } from 'min-dom';
 
@@ -13,6 +14,7 @@ import RefactoringsModule from '../../../lib/refactorings';
 import RefactoringsPopupMenuModule from '../../../lib/popup-menu/';
 
 import diagramXML from '../../fixtures/bpmn/simple.bpmn';
+import { waitFor } from '@testing-library/preact';
 
 describe('Popup Menu', function() {
 
@@ -25,7 +27,7 @@ describe('Popup Menu', function() {
   }));
 
 
-  it('should fetch entries', inject(function(elementRegistry, refactoringsPopupMenuProvider, refactorings) {
+  it('should fetch refactorings', inject(function(elementRegistry, refactoringsPopupMenuProvider, refactorings) {
 
     // given
     const task = elementRegistry.get('Task_1');
@@ -39,21 +41,7 @@ describe('Popup Menu', function() {
   }));
 
 
-  it('should fetch entries', inject(async function(elementRegistry, refactoringsPopupMenuProvider, refactorings) {
-
-    // given
-    const task = elementRegistry.get('Task_1');
-    refactorings.getRefactorings = sinon.spy();
-
-    // when
-    await refactoringsPopupMenuProvider.fetchRefactoringActions(task);
-
-    // then
-    expect(refactorings.getRefactorings).to.have.been.calledWith([ task ]);
-  }));
-
-
-  it('should add entries after fetching', inject(async function(elementRegistry, refactoringsPopupMenuProvider, refactorings, popupMenu) {
+  it('should show entries after fetching', inject(async function(elementRegistry, refactoringsPopupMenuProvider, refactorings, popupMenu) {
 
     // given
     const task = elementRegistry.get('Task_1');
@@ -73,6 +61,97 @@ describe('Popup Menu', function() {
       'foo',
       'bar'
     ]);
+  }));
+
+
+  it('should show loading indicator', inject(async function(elementRegistry, refactorings, popupMenu) {
+
+    // given
+    const task = elementRegistry.get('Task_1');
+    refactorings.getRefactorings = () => new Promise(() => {});
+
+    // when
+    popupMenu.open(task, 'refactoring-actions', {
+      position: { x: 0, y: 0 }
+    });
+
+    // then
+    expect(domQuery('.djs-popup-no-results')).to.exist;
+    expect(domQuery('.djs-popup-no-results .cds--inline-loading')).to.exist;
+  }));
+
+
+  it('should show empty state', inject(async function(elementRegistry, refactorings, popupMenu) {
+
+    // given
+    const task = elementRegistry.get('Task_1');
+    refactorings.getRefactorings = () => [];
+
+    // when
+    popupMenu.open(task, 'refactoring-actions', {
+      position: { x: 0, y: 0 }
+    });
+
+    // then
+    expect(domQuery('.djs-popup-no-results')).to.exist;
+    expect(domQuery('.djs-popup-no-results .cds--inline-loading')).not.to.exist;
+  }));
+
+
+  it('should show entries when loading finises', inject(async function(elementRegistry, refactorings, popupMenu) {
+
+    // given
+    const task = elementRegistry.get('Task_1');
+    let resolve;
+    refactorings.getRefactorings = () => new Promise((r) => {resolve = r;});
+    popupMenu.open(task, 'refactoring-actions', {
+      position: { x: 0, y: 0 }
+    });
+
+    // assume
+    expect(domQuery('.djs-popup-no-results')).to.exist;
+    expect(domQuery('.djs-popup-no-results .cds--inline-loading')).to.exist;
+
+    // when
+    resolve([ { id: 'foo', label: 'foo' }, { id: 'bar', label: 'bar' } ]);
+
+    // then
+    waitFor(() => {
+      const entries = [ ...domQueryAll('.djs-popup.refactoring-actions .entry') ];
+
+      expect(entries).to.have.length(2);
+      expect(entries.map(entry => entry.getAttribute('data-id'))).to.eql([
+        'foo',
+        'bar'
+      ]);
+    });
+
+  }));
+
+
+  it('should show empty state when loading finises', inject(async function(elementRegistry, refactorings, popupMenu) {
+
+    // given
+    const task = elementRegistry.get('Task_1');
+    let resolve;
+    refactorings.getRefactorings = () => new Promise((r) => {resolve = r;});
+    popupMenu.open(task, 'refactoring-actions', {
+      position: { x: 0, y: 0 }
+    });
+
+    // assume
+    expect(domQuery('.djs-popup-no-results')).to.exist;
+    expect(domQuery('.djs-popup-no-results .cds--inline-loading')).to.exist;
+
+    // when
+    resolve([ ]);
+
+    // then
+    waitFor(() => {
+      expect(domQuery('.djs-popup-no-results')).to.exist;
+      expect(domQuery('.djs-popup-no-results .cds--inline-loading')).not.to.exist;
+    });
+
   }));
 
 });
