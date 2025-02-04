815f38a4461e8272bda69dd67bbafc0b029148d2
Philipp Fromme <philipp.fromme@camunda.com>
2025-02-04 15:57:38 +0100

    fix: do not pre-fetch refactorings
    Closes #30

--- test/spec/context-pad/RefactoringsContextPadProvider.spec.js
+++ test/spec/context-pad/RefactoringsContextPadProvider.spec.js
@@ -64,21 +64,6 @@ describe('Context Pad', function() {
   }));
 
 
-  it('should pre-fetch results in popup menu provider', inject(function(elementRegistry, contextPad, refactorings) {
-
-    // given
-    const task = elementRegistry.get('Task_1');
-
-    refactorings.getRefactorings = sinon.spy();
-
-    // when
-    contextPad.open(task);
-
-    // then
-    expect(refactorings.getRefactorings).to.have.been.calledWith([ task ]);
-  }));
-
-
   it('should open popup menu on click', inject(async function(elementRegistry, contextPad, popupMenu) {
 
     // given
